4e6124f9d0ee81ce97c5cfa106ed8bd5ede06d5a
Maks Pleskach <pro1@coloris.com.ua>
2019-09-20 10:31:48 +0300

    change table for index.html, add links menu in other pages, add thanks page

--- values/values.js
+++ values/values.js
@@ -1,11 +1,16 @@
 let linkArr = document.querySelectorAll('.menu-likes__item');
 let infoBlock = document.querySelector('.values-info');
+let button = document.querySelector('.like-button');
+let buttonBack = document.querySelector('.button-back');
 
 for (let i = 0; i < linkArr.length; i++)
 {
     linkArr[i].addEventListener('click', (event) =>
     {
-        infoBlock.style.display = 'block'
+        infoBlock.style.display = 'block';
+        button.style.display = 'none';
+        buttonBack.style.display = 'block';
+
         for (let i = 0; i < linkArr.length; i++)
         {
             linkArr[i].style.display = 'none';
